972bf44ab7f022a2d2b3fd914bdbc1d7c3ab7dd1
Al Francis <fr4nc1stein@gmail.com>
2020-01-17 19:27:16 +0800

    restructure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  # Add a root route if you don't have one...
+	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
+	#root to: 'users#new'
+  	
+  # sign up page with form:
+	get 'users/new' => 'users#new', as: :new_user
+	
+	# create (post) action for when sign up form is submitted:
+	post 'users' => 'users#create'
   get 'home/index', as: "index"
   get 'home/icon', as: "icon"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
+  
   root 'home#index'
 end
