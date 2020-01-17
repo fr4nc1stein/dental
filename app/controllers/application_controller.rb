b0377da22eabb0b5f1a48bfef0e638fb250711ae
Al Francis <fr4nc1stein@gmail.com>
2020-01-17 20:07:45 +0800

    add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception  
+  # Make the current_user method available to views also, not just controllers:
   helper_method :current_user
+  
+  # Define the current_user method:
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
+    # Look up the current user based on user_id in the session cookie:
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
